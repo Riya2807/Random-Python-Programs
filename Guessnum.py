9ec3379bf36f205070773c395648ae58e6a53eba
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2021-10-01 22:59:31 +0530

    Add files via upload

---
+++ Guessnum.py
@@ -0,0 +1,20 @@
+gnum=9
+n=28
+while(gnum>0):
+    num= int(input("Guess the number:\n"))
+    gnum=gnum-1
+    print("Number of guesses left = ",gnum)
+    if num>n:
+        print("Enter a smaller number:\n")
+        continue
+
+    elif num==n:
+        print("Congrats! You guessed the number correctly\n")
+        print("You guessed the number correctly in",gnum-1,"guesses")
+        break
+
+    elif num<n:
+        print("Enter a larger number:\n")
+        continue
+        
+print("Game Over!!!!!")
