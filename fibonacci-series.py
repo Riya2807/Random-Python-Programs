1761632fc837952273e7493e3fc5d3575335919f
Riya Godiyal <58070197+Riya2807@users.noreply.github.com>
2021-10-08 23:52:58 +0530

    Delete fibonacci-series.py

--- fibonacci-series.py
+++
@@ -1,9 +0,0 @@
-def fibonacci_series(n):
-    if n==1:
-        return 0
-    elif n==2:
-        return 1
-    else:
-        return fibonacci_series(n-1)+fibonacci_series(n-2)
-n=int(input("Enter a number:"))
-print(fibonacci_series(n))
